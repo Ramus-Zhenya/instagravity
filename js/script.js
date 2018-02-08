bff280828ce701ae74d29c0d9e87bf4353eaac09
Ramus-Zhenya <30467251+Ramus-Zhenya@users.noreply.github.com>
2018-02-08 14:27:25 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+
+//$(document).ready(function(){
+//  $(".owl-carousel").owlCarousel();
+//});
+$(document).ready(function() {
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+
+    margin:10,
+
+	items:3,
+    responsive:{
+        0:{
+            items:1
+        },
+		467:{
+            items:1
+        },
+        767:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1400 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 100);
+}
+
+var deadline="January 01 2018 00:00:00 GMT+0300"; //for Ukraine
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('clockdiv', deadline);
+	});

8c2be5f5e8b560f9c29a7f68ac8c903b8d367c67
Ramus-Zhenya <30467251+Ramus-Zhenya@users.noreply.github.com>
2018-02-09 09:22:00 +0300

    commit6

--- js/script.js
+++ js/script.js
@@ -9,23 +9,27 @@ return false;
 //});
 $(document).ready(function() {
 	$('.owl-carousel').owlCarousel({
-    loop:true,
-
-    margin:30,
-
+    loop:false,
+	margin:30,
 	items:3,
     responsive:{
         0:{
-            items:1
+            items:1,
+			nav:true,
+			dots: false,
         },
 		467:{
-            items:1
+            items:1,
+			dots: false,
+			 navText: ["<", ">"],
+			nav:true
         },
         767:{
             items:2
         },
         1000:{
-            items:3
+            items:3,
+//			loop:true
         }
     }
 });
